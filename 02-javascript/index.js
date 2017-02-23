d1102ba8a842fb4e928ff8f59a1e24c4c3b3e2d6
Alex Schulz N <aschulzn@gmail.com>
2017-02-22 22:56:28 -0300

    Cambios en la descripción del problema 2

--- 02-javascript/index.js
+++ 02-javascript/index.js
@@ -23,15 +23,26 @@ function requester(method, base, headers = { Accept: '*/*' }) {
     .then(r => r.json());
 }
 
-console.log('')
+console.log(`- What it does?
+Hace llamadas a endpoints utilizando distintos métodos http.
+El único parametro requerido es method. Una vez ejecutada la función, devuelve otra función que realizada la llamada al endpoint.
+Esta función concatena los elementos de un arreglo llamado path con el  parámetro 'base' de la función anterior para armar la URL absoluta.
+`)
+
+console.log(`- How it is called this design pattern or technique?
+Se usa ES6 y la técnica es programación funcional.`)
+
 requester('GET', 'https://api.github.com/users/mediastream')().then(function(data){
   console.log('Caso 1, llamada directa al endpoint');
+  console.log(`requester('GET', 'https://api.github.com/users/mediastream')()`);
 });
 
 requester('GET', 'https://api.github.com')(['users', 'mediastream']).then(function(data) {
   console.log('Caso 2: Llamada al endpoint a través de la concatenación de la base con el path');
+  console.log(`requester('GET', 'https://api.github.com')(['users', 'mediastream'])`);
 });
 
 requester('GET')(['https://api.github.com', 'users', 'mediastream']).then(function(data){
   console.log('Case 3: Llamada al endpoint a través de la concatenación de todos los parametros del path');
+  console.log(`requester('GET', 'https://api.github.com')(['users', 'mediastream'])`);
 })
